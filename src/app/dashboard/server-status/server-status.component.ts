c129f5334b2b2a74d9c44920d5f7450cc10eab8d
Petre Dragos Teodor <petredragosteodor@gmail.com>
2026-01-26 15:44:42 +0200

    Bits and pieces

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+currentStatus: 'online' | 'offline' | 'unknown'= 'offline';
+private destroyRef = inject(DestroyRef);
+
+  ngOnInit()
+{
+   
+      const rnd = Math.random();
+    if (rnd < 0.5) {
+      this.currentStatus = 'online';
+    } else if(rnd < 0.9) {
+      this.currentStatus = 'offline';
+    } else {
+      this.currentStatus = 'unknown';
+    }
+  
+  
+}
+  // ngOnDestroy() {
+  //   clearTimeout(this.interval);
+  // }
+
 }
